07a15896de893ca17b51c2fedb3e3e5d21602370
Pratibha Sane <pratibha@Pratibhas-MacBook-Pro.local>
2018-03-21 18:37:13 +0530

    initial set up project

---
+++ SampleGitDemo/SampleGitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleGitDemo
+//
+//  Created by Pratibha Sane on 21/03/18.
+//  Copyright © 2018 Pratibha Sane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
